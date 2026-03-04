dde137190eb2c22617066d6e3219311e0c3f0f19
sokolLonely <simonryab@gmail.com>
2026-03-04 11:36:38 -0700

    task class done

--- week8/src/edu/ucalgary/oop/Task.java
+++ week8/src/edu/ucalgary/oop/Task.java
@@ -3,7 +3,16 @@ package edu.ucalgary.oop;
 import java.util.Objects;
 
 public class Task {
+    private int id;
+    private String title;
+    private boolean isCompleted;
+    public Task(int id, String title, boolean isCompleted)
+    {
+        this.id = id;
+        this.isCompleted = isCompleted;
+        this.title = String.valueOf(title);
 
+    }
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -17,5 +26,9 @@ public class Task {
     @Override
     public int hashCode() {
         return Objects.hash(id, title, isCompleted);
+    }
+    
+    public Task copy(){
+           return  new Task(this.id, this.title, this.isCompleted);     
     }
 }
