930058e0cc8268f5d3c9e77ada5058e3879313cc
Arjun Chatha <arjunchatha13@gmail.com>
2026-03-18 12:41:27 -0600

    Update SmartDevice.java

--- week10/src/edu/ucalgary/oop/SmartDevice.java
+++ week10/src/edu/ucalgary/oop/SmartDevice.java
@@ -1,31 +1,22 @@
-
 package edu.ucalgary.oop;
-
 
 public abstract class SmartDevice<T> implements Observer {
     private T state;
 
-   
     public SmartDevice(T initialState) {
-        
         this.state = initialState;
     }
 
-    
     public T getState() {
         return this.state;
     }
 
-
     public void setState(T state) {
         this.state = state;
-    
     }
 
-
     @Override
-    public abstract void update(int temperature);
-
-
+    public abstract void update(String message);
     
+    public abstract void performAction(); 
 }
