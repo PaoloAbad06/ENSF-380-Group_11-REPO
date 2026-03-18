0294abb9882cd282c0790b42d9a82e8bd073c43c
Arjun Chatha <arjunchatha13@gmail.com>
2026-03-18 12:41:58 -0600

    Update SmartLight.java

--- week10/src/edu/ucalgary/oop/SmartLight.java
+++ week10/src/edu/ucalgary/oop/SmartLight.java
@@ -1,26 +1,23 @@
-// TODO: package, any includes, class declaration, constructor
-//       and the methods performAction and update.
+package edu.ucalgary.oop;
 
+public class SmartLight extends SmartDevice<Boolean> {
+
+    public SmartLight() {
+        super(false); // Default to OFF
+    }
 
     @Override
     public void update(String message) {
-        switch (message) {
-            case "Sleep":
-                setState(false);
-                break;
-            case "Vacation":
-                setState(Math.random() < 0.5); // Randomly turn ON/OFF
-                break;
-            default:
-                System.out.println("SmartLight: Unknown message - " + message);
+        if (message.equalsIgnoreCase("Sleep")) {
+            setState(false);
+        } else if (message.equalsIgnoreCase("Vacation")) {
+            setState(Math.random() < 0.5); 
         }
+        performAction();
     }
 
-    public void dim(int percentage) {
-        if (getState()) {
-            System.out.println("Dimming the light to " + percentage + "%.");
-        } else {
-            System.out.println("Cannot dim the light. It is currently OFF.");
-        }
+    @Override
+    public void performAction() {
+        System.out.println("Light is " + (getState() ? "ON" : "OFF"));
     }
 }
