cc5cb2fd07b84570c00f76fcfa3dc66f38f0fc36
Arjun Chatha <arjunchatha13@gmail.com>
2026-03-18 11:57:31 -0600

    SmartLock.java done

--- week10/src/edu/ucalgary/oop/SmartLock.java
+++ week10/src/edu/ucalgary/oop/SmartLock.java
@@ -1,7 +1,50 @@
 // TODO: package, any includes, class declaration, constructor
 //       and the methods performAction and update.
 
+package edu.ucalgary.oop;
+
+import java.util.Observer;
+
+public class SmartLock extends SmartDevice<Boolean> implements Observer {
+
+    // Constructor
+    public SmartLock(String name) {
+        super(name);
+        this.setState(true); 
+    }
+
+    @Override
+    public void update(String message) {
+        
+        if (message.equalsIgnoreCase("Sleep") || message.equalsIgnoreCase("Vacation")) {
+            
+            setState(true); 
+        }
+        
+        performAction();
+    }
+
+    @Override
+    public void performAction() {
+
+        String status;
+
+        if (getState() == true) {
+            status = "LOCKED";
+            } 
+        
+        else {
+           
+            status = "UNLOCKED";
+        }
+
+        System.out.println("SmartLock " + getName() + " is now " + status + ".");
+
+    }
+
+    
     public void autoLock(int delayInSeconds) {
+       
         System.out.println("Auto-lock enabled. Door will lock in " + delayInSeconds + " seconds.");
         new Thread(() -> {
             try {
