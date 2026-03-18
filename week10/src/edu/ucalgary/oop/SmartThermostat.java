ec340254a7523ed5e5f02a1136c2e78fd89878e9
Arjun Chatha <arjunchatha13@gmail.com>
2026-03-18 11:46:12 -0600

    SmartThermostat.java done

--- week10/src/edu/ucalgary/oop/SmartThermostat.java
+++ week10/src/edu/ucalgary/oop/SmartThermostat.java
@@ -1,6 +1,32 @@
 // TODO: package, any includes, class declaration, constructor
 //       and the methods performAction and update.
 
+
+package edu.ucalgary.oop;
+
+
+public class SmartThermostat extends SmartDevice<Integer> {
+
+    public SmartThermostat() {
+        // default temp
+        super(20);
+    }
+
+    @Override
+    public void update(String message) {
+        
+        if (message.equalsIgnoreCase("Sleep")) {
+            adjustTemperature(18); 
+        } else if (message.equalsIgnoreCase("Vacation")) {
+            adjustTemperature(20); 
+        }
+    }
+
+    @Override
+    public void performAction() {
+        System.out.println("Thermostat is maintaining temperature at " + getState() + "°C.");
+    }
+
     public void adjustTemperature(int desiredTemp) {
         int currentTemp = getState();
         int difference = desiredTemp - currentTemp;
@@ -16,3 +42,4 @@
         setState(desiredTemp);
     }
 }
+
